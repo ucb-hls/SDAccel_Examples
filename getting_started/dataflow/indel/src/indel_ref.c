c261e2bbcfff9803f693339e04b24b6f63090e1d
Cloud User <qijing.huang@berkeley.edu>
2018-01-30 19:30:38 +0000

    Commit working code

--- getting_started/dataflow/indel/src/indel_ref.c
+++ getting_started/dataflow/indel/src/indel_ref.c
@@ -8,6 +8,8 @@ void whd (char* consensus, const int consensus_size, int* consensus_length, \
         int reads_base = 0;
         for (j = 0; j < reads_size; j++) {
             int local_reads_length = reads_length[j];
+            fprintf(stderr, "consensus size %d i %d consensus length %d, read size %d j %d reads length %d\n", \
+                consensus_size, i, local_consensus_length, reads_size,  j, local_reads_length);
             int min = 0x7fffffff; 
             int min_idx = local_consensus_length - local_reads_length + 1;
             for (k = 0; k <= local_consensus_length - local_reads_length; k++) {
@@ -15,17 +17,29 @@ void whd (char* consensus, const int consensus_size, int* consensus_length, \
                 // whd 
                 int whd = 0;
                 // Optimization tree based reduction
-                for (l = 0; l <= reads_length[j]; l++) {
+                for (l = 0; l < reads_length[j]; l++) {
+
+                    //printf("%c", consensus[consensus_base + k + l]);
+                    //printf("%c", reads[reads_base + k + l]);
                     if (consensus[consensus_base + k + l] != reads[reads_base + l]){
                         whd += qs[reads_base + l];
+                        //if(k == 8 & j == 1){
+                        //    printf("whd: %d\t", whd);
+                        //}
                     }                        
+                    //printf("whd: %d\t", whd);
+                    //printf("\t");
+
                 }
+
+                //printf("\n");
                 if (whd < min) {
                     min =  whd; 
                     min_idx = k; 
                }
 
             }
+            fprintf(stderr, "min_idx %d\n", min_idx);
             assert(min_idx <= local_consensus_length - local_reads_length);
             
             min_whd[i * reads_size + j] = min;
@@ -36,7 +50,20 @@ void whd (char* consensus, const int consensus_size, int* consensus_length, \
     }
 }
 
+void print_whd (int* min_whd, int consensus_size, int reads_size) {
+    int i, j;
+    fprintf(stderr, "whd: \n");
+    for (i = 0; i < reads_size; i ++) {
+        for (j = 0; j < consensus_size; j ++) {
+            int tmp = min_whd[j * reads_size + i];
+            fprintf(stderr, "%d\t", tmp);
+        }
+        fprintf(stderr, "\n");
+    }
+}
+
 void score_whd (int* min_whd, int* min_whd_idx, int consensus_size, int reads_size, int* new_ref, int* new_ref_idx) {
+    print_whd(min_whd, consensus_size, reads_size);
     // might need to reduce the bits used in here 
     assert(new_ref_idx != NULL); 
     assert(min_whd != NULL); 
@@ -53,17 +80,17 @@ void score_whd (int* min_whd, int* min_whd_idx, int consensus_size, int reads_si
         min_idx = (score < min_score) ? i : min_idx;
         //scores[i] = score;
     }
-    printf("min_idx: %d\n", min_idx);
+    fprintf(stderr, "min_idx: %d\n", min_idx);
     assert(min_idx < consensus_size);
     
     for (j = 0; j < reads_size; j++) {
-        if ( min_whd[ min_idx * reads_size + j] < min_whd[j]){
+        //if ( min_whd[ min_idx * reads_size + j] < min_whd[j]){
             new_ref[j] = min_whd[min_idx * reads_size + j];
             new_ref_idx[j] = min_whd_idx[ min_idx * reads_size +j];
-        } else{
-            new_ref[j] = min_whd [j];
-            new_ref_idx[j] = min_whd_idx[j]; 
-        }
+        //} else{
+        //    new_ref[j] = min_whd [j];
+        //    new_ref_idx[j] = min_whd_idx[j]; 
+        //}
     }
 
     for (j = 0; j < reads_size; j++) {
