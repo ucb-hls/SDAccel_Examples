7396f2663eb44b1892736432fc97b748ecea29ff
Cloud User <qijing.huang@berkeley.edu>
2018-02-06 22:13:18 +0000

    Added new folder for optimized indel implementation

---
+++ getting_started/dataflow/indel_opt/src/Indel_Accel.cpp
@@ -0,0 +1,142 @@
+#include <hls_stream.h>
+#include <stdio.h>
+#include <string.h>
+#include <ap_int.h>
+
+#include "Indel_Accel.h"
+//#define CON_SIZE 2
+//#define READS_SIZE 8
+
+// JENNY TODO
+// ap_uint<3> for ATGCU 
+// ap_uint<4> to round it to power of 2 
+// ap_uint<512> is the most efficient for global memory 
+// Assign reads and consensus to different bundles for memory banking optimizations
+//
+extern "C" {
+//whd(con_arr, con_size, con_len, reads_arr, reads_size, reads_len, weights_arr, min_whd, min_whd_idx);
+void Indel_Accel (ap_uint<4>* consensus, const int consensus_size, int* consensus_length, \
+    ap_uint<4>* reads, const int reads_size, int* reads_length, ap_uint<4>* qs, int* new_ref_idx) {
+#pragma HLS INTERFACE m_axi port=consensus offset=slave bundle=gmem
+#pragma HLS INTERFACE m_axi port=consensus_length offset=slave bundle=gmem
+#pragma HLS INTERFACE m_axi port=reads offset=slave bundle=gmem
+#pragma HLS INTERFACE m_axi port=reads_length offset=slave bundle=gmem
+#pragma HLS INTERFACE m_axi port=qs offset=slave bundle=gmem
+#pragma HLS INTERFACE m_axi port=new_ref_idx offset=slave bundle=gmem
+
+#pragma HLS INTERFACE s_axilite port=consensus bundle=control
+#pragma HLS INTERFACE s_axilite port=consensus_size bundle=control
+#pragma HLS INTERFACE s_axilite port=consensus_length bundle=control
+#pragma HLS INTERFACE s_axilite port=reads bundle=control
+#pragma HLS INTERFACE s_axilite port=reads_size bundle=control
+#pragma HLS INTERFACE s_axilite port=reads_length bundle=control
+#pragma HLS INTERFACE s_axilite port=qs bundle=control
+#pragma HLS INTERFACE s_axilite port=new_ref_idx bundle=control
+#pragma HLS INTERFACE s_axilite port=return bundle=control
+
+
+    //array of stream declaration
+    /*hls::stream<ap_uint<4>> condStreams[CON_SIZE];
+    //#pragma HLS STREAM variable=condStreams depth=32
+
+    hls::stream<ap_uint<4>> readStreams[READS_SIZE];
+    //#pragma HLS STREAM variable=readStreams depth=32
+
+
+    #pragma HLS dataflow
+    for (int i = 0 ; i < CON_SIZE; i ++){
+        read_input(consensus, condStreams[i], consensus_length[i]);
+    }
+   
+    #pragma HLS dataflow
+    for (int i = 0 ; i < READS_SIZE; i ++){
+        read_input(reads, readStreams[i], reads_length[i]);
+    }*/
+    int min_whd[CON_SIZE * READS_SIZE];
+    int min_whd_idx[CON_SIZE * READS_SIZE];
+    int new_ref[READS_SIZE];
+    int consensus_base = 0; 
+    int i, j, k, l;
+    for (i = 0; i < consensus_size; i++) {
+        int local_consensus_length =  consensus_length[i];
+        int reads_base = 0;
+        for (j = 0; j < reads_size; j++) {
+            int local_reads_length = reads_length[j];
+            //printf( "consensus size %d i %d consensus length %d, read size %d j %d reads length %d\n", \
+                consensus_size, i, local_consensus_length, reads_size,  j, local_reads_length);
+            int min = 0x7fffffff; 
+            int min_idx = local_consensus_length - local_reads_length + 1;
+            for (k = 0; k <= local_consensus_length - local_reads_length; k++) {
+
+                // whd 
+                int whd = 0;
+                // Optimization tree based reduction
+                for (l = 0; l < reads_length[j]; l++) {
+
+                    //printf("%c", consensus[consensus_base + k + l]);
+                    //printf("%c", reads[reads_base + k + l]);
+                    if (consensus[consensus_base + k + l] != reads[reads_base + l]){
+                        whd += qs[reads_base + l];
+                        //if(k == 8 & j == 1){
+                        //    printf("whd: %d\t", whd);
+                        //}
+                    }                        
+                    //printf("whd: %d\t", whd);
+                    //printf("\t");
+
+                }
+
+                //printf("\n");
+                if (whd < min) {
+                    min =  whd; 
+                    min_idx = k; 
+               }
+
+            }
+            //printf( "min_idx %d\n", min_idx);
+            //assert(min_idx <= local_consensus_length - local_reads_length);
+            
+            //min_whd[i][j] = min;
+            //min_whd_idx[i][j] = min_idx;
+            min_whd[i * reads_size + j] = min;
+            min_whd_idx[i * reads_size + j] = min_idx;
+ 
+            reads_base += local_reads_length;
+        }
+        consensus_base += local_consensus_length;
+    }
+    
+    int min_score = 0x7fffffff;
+    int min_idx = consensus_size + 1;
+    for (i = 1; i < consensus_size; i++) {
+        int score = 0;
+        for (j = 0; j < reads_size; j++) {
+            int tmp = min_whd[i * reads_size + j] - min_whd[j];
+            score += (tmp > 0) ? tmp: -tmp;
+        }
+        min_idx = (score < min_score) ? i : min_idx;
+        //scores[i] = score;
+    }
+    //printf( "min_idx: %d\n", min_idx);
+    //assert(min_idx < consensus_size);
+    
+    for (j = 0; j < reads_size; j++) {
+        //if ( min_whd[ min_idx * reads_size + j] < min_whd[j]){
+            //new_ref[j] = min_whd[min_idx][j];
+            //new_ref_idx[j] = min_whd_idx[min_idx][j];
+            new_ref[j] = min_whd[min_idx * reads_size + j];
+            new_ref_idx[j] = min_whd_idx[ min_idx * reads_size +j];
+ 
+        //} else{
+        //    new_ref[j] = min_whd [j];
+        //    new_ref_idx[j] = min_whd_idx[j]; 
+        //}
+    }
+    for (j = 0; j < reads_size; j++) {
+     printf("Read %2d whd %2d index %2d\n", j, new_ref[j], new_ref_idx[j]);
+    }
+
+}
+
+}
+
---
+++ getting_started/dataflow/indel_opt/src/ref/main.c
@@ -0,0 +1,197 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include "indel_ref.c"
+
+int* read_target (){
+
+  FILE *fp;
+  char* con = "./1025.TARGET.tbl"; 
+  fp=fopen(con, "r");
+
+  if (fp == NULL)
+    exit(EXIT_FAILURE);
+
+  char line[2046];
+  char separators[] = "|";
+
+  int* target = malloc(sizeof(int)*4);
+
+  fgets(line, sizeof(line), fp);
+  char *p = strtok(line, separators); 
+
+  int i = 0; 
+
+  while (p != NULL){
+    target[i++] = atoi(p); 
+    //printf("%s\n", p);
+    p = strtok(NULL, separators);
+  } 
+  printf("%d %d\n", i, target[3]);
+  fclose(fp);
+
+  int target_num = target[0];
+  int contig_num = target[1];
+  int starting_pos = target[2];
+  int ending_pos = target[3];
+
+  int length = ending_pos - starting_pos;
+  //printf("length: %d\n", length);
+
+  return target;
+}
+
+int count_lines(char* filename){
+
+  FILE *fp;
+  fp = fopen(filename, "r");
+
+  // Check if file exists
+  if (fp == NULL)
+  {
+    printf("Could not open file %s\n", filename);
+    return 0;
+  }
+  int count = 0;
+  char c; 
+  // Extract characters from file and store in character c
+  char *line = NULL;
+  size_t line_len = 0;
+  ssize_t read;
+ 
+  while ((read = getline(&line, &line_len, fp)) != -1) {
+  //for (c = getc(fp); c != EOF; c = getc(fp))
+  //  if (c == '\n') // Increment count if this character is newline
+      count = count + 1;
+  }
+  // Close the file
+  fclose(fp);
+  return count;
+}
+
+void parse(const char* file_prefix, int col_num, char* con_arr, int** con_len_arr, int* con_size) {
+
+  FILE *fp;
+  char con[256]="";
+  strcat(con, file_prefix);
+  strcat(con, ".tbl");
+
+  int num_lines = count_lines(con);
+  //printf("num_lines %d\n", num_lines);
+  fp=fopen(con, "r");
+
+  if (fp == NULL)
+    exit(EXIT_FAILURE);
+
+  // Length of each segment 
+  int * con_len = (int*) malloc(sizeof(int) * num_lines);
+  assert(con_len != NULL);
+  * con_len_arr = con_len;
+  // Location of the end of each segment  
+  int* pos = (int*) malloc(sizeof(int) * num_lines);
+  assert(pos != NULL);
+
+  char separators[] = "|";
+  char *line = NULL;
+  size_t line_len = 0;
+  ssize_t read;
+
+  int line_num = 0;
+  int base = 0;
+  while ((read = getline(&line, &line_len, fp)) != -1) {
+    //printf("Retrieved line of length %zu :\n", read);
+    //printf("%s", line);
+
+    char *p = strtok(line, separators); 
+
+    assert (p != NULL);
+
+    int i = 1; 
+    while (p != NULL){
+      //printf("%s\n", p);
+      //target[i++] = atoi(p); 
+      p = strtok(NULL, separators);
+
+      if(i == col_num){ 
+        //if(line_num == 0){
+        //  len = strlen(p); 
+        //  con_arr = (char*) malloc(len * num_lines * sizeof(char));
+        //}
+        con_len[line_num] = strlen(p);
+        //fprintf(stderr, "line_num %d strlen %d\n", line_num, con_len[line_num]);
+        int k; 
+        for (k = 0; k < con_len[line_num]; k++){
+          //printf("%c", p[k]);
+          strncpy(&con_arr[base + k], &p[k], 1);
+          //printf("%d\n", line_num* len + k);
+          fprintf(stderr, "%c", con_arr[base + k]);
+        }
+        fprintf(stderr, "\n");
+      }
+      i++;
+    }
+    base += con_len[line_num];
+    pos[line_num] = base; 
+    line_num++;
+  }
+  //printf("%d\n", len);
+  //int arr_size = len * num_lines;
+  //printf("%d\n", arr_size);
+  //for(int i= 0; i < arr_size; i++ ){
+  //  printf("%d ", con_arr[i]);
+  //}
+
+  *con_size = num_lines;
+  free(line);
+  fclose(fp); 
+}
+
+
+#define NUM_CON 32
+#define NUM_READ 256
+#define CON_LEN 2048
+#define READ_LEN 256
+
+int main(int argc, char* argv[]) {
+  if (argc < 2){
+        printf("Please specify filename! \n");
+        return 1;
+  }
+  //const char* file_prefix = "./ir_toy/25";
+  const char* file_prefix = "./ir_toy/";
+  char con[256]="";
+  strcat(con, file_prefix);
+  strcat(con, argv[1]);
+  strcat(con, ".SEQ");
+
+  char reads[256]="";
+  strcat(reads, file_prefix);
+  strcat(reads, argv[1]);
+  strcat(reads, ".READS");
+
+  printf("TARGET %s\n", argv[1]);
+  char* con_arr, *reads_arr, *weights_arr; 
+  int *con_len, con_size, *reads_len, reads_size; 
+
+  // Malloc the largest array 
+  con_arr = (char*) malloc( NUM_CON * CON_LEN * sizeof(char));
+  reads_arr = (char*) malloc( NUM_READ * READ_LEN * sizeof(char));
+  weights_arr = (char*) malloc( NUM_READ * READ_LEN * sizeof(char));
+
+  parse( con,1, con_arr, &con_len, &con_size);
+  parse( reads, 4, reads_arr, &reads_len, &reads_size);
+  parse( reads, 5, weights_arr, &reads_len, &reads_size);
+   
+  int* min_whd = (int*) malloc(con_size * reads_size * sizeof(int));
+  int* min_whd_idx = (int*) malloc(con_size * reads_size * sizeof(int));
+  int* new_ref= (int*) malloc(reads_size * sizeof(int));
+  int* new_ref_idx= (int*) malloc(reads_size * sizeof(int));
+
+  whd(con_arr, con_size, con_len, reads_arr, reads_size, reads_len, weights_arr, min_whd, min_whd_idx);
+  score_whd (min_whd,  min_whd_idx, con_size, reads_size, new_ref, new_ref_idx);
+ 
+  return 0;
+
+}
+
