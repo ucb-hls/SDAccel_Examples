b7e54e9c0d9d9923576e1129f80f14f564e83dd4
Cloud User <qijing.huang@berkeley.edu>
2018-03-16 05:36:27 +0000

    Add update makefile

---
+++ getting_started/dataflow/indel_opt_8/create_afi.sh
@@ -0,0 +1,5 @@
+BM=Indel_Accel.hw.xilinx_aws-vu9p-f1_4ddr-xpr-2pr_4_0
+#XCLBIN=/home/centos/aws-fpga/SDAccel/examples/xilinx/getting_started/dataflow/indel/xclbin/
+XCLBIN=./xclbin/
+$SDACCEL_DIR/tools/create_sdaccel_afi.sh -xclbin=$XCLBIN/$BM.xclbin -o=$BM \
+        -s3_bucket="hqjenny-100" -s3_dcp_key=dcp/$BM-4krnl -s3_logs_key=logs/
